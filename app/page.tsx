e2c1a9670e591c4ae2d4d5f49f5ae7ce576215bf
Diego Vilhalva <diegorocha450@yahoo.com>
2025-08-15 10:33:33 -0300

    add hero ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
-import { Button } from "@/components/ui/button";
+import Hero from "./_components/Hero";
+import {PopularCityList} from "./_components/PopularCityList"
 
 
 export default function Home() {
   return (
     <div className="">
-        
+      <Hero />        
+      <PopularCityList />
     </div>
   );
 }
